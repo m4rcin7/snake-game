8af68e717dbc383e8d50fe01bb82166df7cb61f3
Marcin Kopatys <mrku1337@gmail.com>
2025-10-19 14:50:52 +0200

    types-app

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Point = { x: number; y: number };
+export type Direction = "UP" | "DOWN" | "LEFT" | "RIGHT";
+
+export interface GameState {
+  snake: Point[];
+  food: Point;
+  direction: Direction;
+  score: number;
+  isGameOver: boolean;
+}
